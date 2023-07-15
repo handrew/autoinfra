764b275b176d5170ab4642d7967912ad76a1dc5a
Andrew Han <handrew11@gmail.com>
2023-07-15 13:42:13 -0700

    add exec remote command

--- infra_gpt.py
+++ infra_gpt.py
@@ -29,6 +29,12 @@ def ssh_into_instance(hostname, username):
     return ssh
 
 
+def execute_remote_commmand(ssh, command):
+    """Execute a remote command on an SSH connection."""
+    stdin, stdout, stderr = ssh.exec_command(command)
+    return stdout.readlines()
+
+
 def main():
     actions = [
         Action(
@@ -53,5 +59,4 @@ and make sure that one server is running at all times."""
 
 
 if __name__ == "__main__":
-    ssh = ssh_into_instance(hostname="164.92.91.161", username="root")
-    import pdb; pdb.set_trace()
+    pass
